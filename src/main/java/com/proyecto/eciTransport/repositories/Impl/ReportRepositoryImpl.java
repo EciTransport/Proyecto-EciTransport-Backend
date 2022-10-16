b5811174af6addec7b769444d5be45f36be3c853
Yesid Mora <yesid.mora@mail.escuelaing.edu.co>
2022-10-16 17:39:55 -0500

    Reportes quemados

--- src/main/java/com/proyecto/eciTransport/repositories/Impl/ReportRepositoryImpl.java
+++ src/main/java/com/proyecto/eciTransport/repositories/Impl/ReportRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.proyecto.eciTransport.models.ReportModel;
 import com.proyecto.eciTransport.repositories.ReportRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 @Repository
@@ -13,5 +14,11 @@ public class ReportRepositoryImpl implements ReportRepository {
 
     public ReportRepositoryImpl() {
         //CreateReports
+        ReportModel reportModel1 = new ReportModel(1, "Cesar", " Accidente calle 187", new Timestamp(0));
+        ReportModel reportModel2 = new ReportModel(2, "Cesar", " Accidente calle 222", new Timestamp(0));
+        ReportModel reportModel3 = new ReportModel(3, "Esteban", " Accidente calle 200", new Timestamp(0));
+        reports.add(reportModel1);
+        reports.add(reportModel2);
+        reports.add(reportModel3);
     }
 }
