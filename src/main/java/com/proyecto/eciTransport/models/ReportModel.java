f8cac8cf2bdc78ef4a390731d2bc8501160aa4bc
Yesid Mora <yesid.mora@mail.escuelaing.edu.co>
2022-10-16 17:36:29 -0500

    Reports

--- src/main/java/com/proyecto/eciTransport/models/ReportModel.java
+++ src/main/java/com/proyecto/eciTransport/models/ReportModel.java
@@ -1,4 +1,50 @@
 package com.proyecto.eciTransport.models;
 
+import java.sql.Timestamp;
+
 public class ReportModel {
+
+    private long id;
+    private String author;
+    private String description;
+    private Timestamp hourReport;
+
+    public ReportModel(long id, String author, String description, Timestamp hourReport) {
+        this.id = id;
+        this.author = author;
+        this.description = description;
+        this.hourReport = hourReport;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getHourReport() {
+        return hourReport;
+    }
+
+    public void setHourReport(Timestamp hourReport) {
+        this.hourReport = hourReport;
+    }
 }
--- src/main/java/com/proyecto/eciTransport/repositories/Impl/ReportRepositoryImpl.java
+++ src/main/java/com/proyecto/eciTransport/repositories/Impl/ReportRepositoryImpl.java
@@ -1,6 +1,17 @@
 package com.proyecto.eciTransport.repositories.Impl;
 
+import com.proyecto.eciTransport.models.ReportModel;
 import com.proyecto.eciTransport.repositories.ReportRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
+@Repository
 public class ReportRepositoryImpl implements ReportRepository {
+
+    private ArrayList<ReportModel> reports = new ArrayList<>();
+
+    public ReportRepositoryImpl() {
+        //CreateReports
+    }
 }
